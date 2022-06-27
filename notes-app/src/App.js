4ba5f116c3d6ca2ad68b2f1d0ff78e21d3e9340f
kgni <admin@mdia.dk>
2022-06-27 15:43:32 +0200

    add server-json and axios for fetching notes from the server. Add useEffect for fetching the data and putting the data into state

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,16 +1,61 @@
-import Note from './components/Note'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Note from './components/Note';
 
-const App = ({ notes } ) => {
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-    </div>
-  )
-}
+const App = () => {
+	const [notes, setNotes] = useState([]);
+	const [newNote, setNewNote] = useState('a new note...');
+	const [showAll, setShowAll] = useState(true);
 
-export default App
+	useEffect(() => {
+		async function fetchNotes() {
+			const response = await axios.get('http://localhost:3001/notes');
+			const data = await response.data;
+			setNotes(data);
+		}
+		fetchNotes();
+	}, []);
+
+	const addNote = (event) => {
+		event.preventDefault();
+		const noteObject = {
+			content: newNote,
+			date: new Date().toISOString(),
+			important: Math.random() < 0.5,
+			id: notes.length + 1,
+		};
+
+		setNotes(notes.concat(noteObject));
+		setNewNote('');
+	};
+	const handleNoteChange = (event) => {
+		console.log(event.target.value);
+		setNewNote(event.target.value);
+	};
+
+	const notesToShow = showAll
+		? notes
+		: notes.filter((note) => note.important === true);
+
+	return (
+		<div>
+			<h1>Notes</h1>
+			<div>
+				<button onClick={() => setShowAll(!showAll)}>
+					show {showAll ? 'important' : 'all'}
+				</button>
+			</div>
+			<ul>
+				{notesToShow.map((note) => (
+					<Note key={note.id} note={note} />
+				))}
+			</ul>
+			<form onSubmit={addNote}>
+				<input onChange={handleNoteChange} value={newNote} />
+				<button type="submit">save</button>
+			</form>
+		</div>
+	);
+};
+
+export default App;
