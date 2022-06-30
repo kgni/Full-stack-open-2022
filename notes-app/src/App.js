56d8b72dbeab5011a0d02b4d9899356d1351a832
kgni <admin@mdia.dk>
2022-06-30 13:02:04 +0200

    add notification component

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -2,11 +2,13 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Note from './components/Note';
 import noteService from './services/notes';
+import Notification from './components/Notification';
 
 const App = () => {
 	const [notes, setNotes] = useState([]);
 	const [newNote, setNewNote] = useState('a new note...');
 	const [showAll, setShowAll] = useState(true);
+	const [errorMessage, setErrorMessage] = useState(null);
 
 	useEffect(() => {
 		noteService.getAll().then((response) => {
@@ -46,7 +48,12 @@ const App = () => {
 				setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
 			})
 			.catch((error) => {
-				alert(`the note '${note.content}' was already deleted from server`);
+				setErrorMessage(
+					`Note '${note.content}' was already removed from server`
+				);
+				setTimeout(() => {
+					setErrorMessage(null);
+				}, 5000);
 				setNotes(notes.filter((n) => n.id !== id));
 			});
 	};
@@ -54,8 +61,9 @@ const App = () => {
 	return (
 		<div>
 			<h1>Notes</h1>
+			<Notification message={errorMessage} />
 			<div>
-				<button onClick={() => setShowAll(!showAll)}>
+				<button className="btn" onClick={() => setShowAll(!showAll)}>
 					show {showAll ? 'important' : 'all'}
 				</button>
 			</div>
