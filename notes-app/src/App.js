bed93057ce6e6704c8d6cb76f56b239a13880fc0
kgni <admin@mdia.dk>
2022-06-30 13:05:55 +0200

    add footer to notes-app

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Note from './components/Note';
 import noteService from './services/notes';
 import Notification from './components/Notification';
+import Footer from './components/Footer';
 
 const App = () => {
 	const [notes, setNotes] = useState([]);
@@ -80,6 +81,7 @@ const App = () => {
 				<input onChange={handleNoteChange} value={newNote} />
 				<button type="submit">save</button>
 			</form>
+			<Footer />
 		</div>
 	);
 };
